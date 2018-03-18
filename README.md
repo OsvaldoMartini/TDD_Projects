991cd974d755c363882d0cf8c74c8f265aa226fc
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-03-18 12:19:49 +0000

    Class Test Name and First Test Method
    Defining  Classes and Interfaces
    Defining the problem to implement

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # TDD_Projects
 
 TDD - Test Driven Development Cases
+
+Following the Cycles
+Red 	 ->  Failing Unit Test
+Green 	 -> Just Enough Code to Pass Test
+Refactor -> (Yellow) Clean Up Code
+ 
---
+++ TDD Case 1/ServiceSunsetCalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDD_Case_1
+{
+    [TestFixture]
+    public class ServiceSunsetCalculatorTests
+    {
+
+        /*Summary
+         Defining the Interfaces and Classes
+         JSON File Response From WebServices
+         good data (good path)
+         {"result":{"sunrise":"6:37:49 AM","sunset":"4:42:49 PM","solar_noon":"11:40:19 AM","day_length":"10:05:00.1530000"},"status":"OK"}
+          
+         * good data (sad path)
+         {"results":null,"status":"ERROR"}
+         */
+
+        [Test]
+        public void ServiceSunsetCalculator_ImplementsISunsetCalculator()
+        {
+            //Arrange values to Initiate de TDD
+            
+            
+            //Actions and/or Methods
+
+
+            //Assert
+
+        }
+    }
+}
