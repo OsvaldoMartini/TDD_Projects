cf1e7847908d5975f1d29bcbf1386e1c9a982b15
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-03-18 16:43:34 +0000

    TDD Red Cycle - Parse Method
    Parse Json and GetData Expected
    Tip for prepare goodData to be read as a Mock in README.md file

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ Following the Cycles
 Red 	 ->  Failing Unit Test
 Green 	 -> Just Enough Code to Pass Test
 Refactor -> (Yellow) Clean Up Code
- 
+	
+	
+Aux Tool will create Scapes in Json File
+https://www.freeformatter.com/java-dotnet-escape.html#ad-output
--- TDD Case 1/ServiceSunsetCalculatorTests.cs
+++ TDD Case 1/ServiceSunsetCalculatorTests.cs
@@ -9,32 +9,43 @@ using NUnit.Framework;
 
 namespace TDD_Case_1
 {
+    /*Summary
+       Defining the Interfaces and Classes
+       JSON File Response From WebServices
+       good data (good path)
+       {"result":{"sunrise":"6:37:49 AM","sunset":"4:42:49 PM","solar_noon":"11:40:19 AM","day_length":"10:05:00.1530000"},"status":"OK"}
+          
+       * good data (sad path)
+       {"results":null,"status":"ERROR"}
+       */
+
     [TestFixture]
     public class ServiceSunsetCalculatorTests
     {
 
-        /*Summary
-         Defining the Interfaces and Classes
-         JSON File Response From WebServices
-         good data (good path)
-         {"result":{"sunrise":"6:37:49 AM","sunset":"4:42:49 PM","solar_noon":"11:40:19 AM","day_length":"10:05:00.1530000"},"status":"OK"}
-          
-         * good data (sad path)
-         {"results":null,"status":"ERROR"}
-         */
+      string goodData =  "{\"result\":{\"sunrise\":\"6:37:49 AM\",\"sunset\":\"4:42:49 PM\",\"solar_noon\":\"11:40:19 AM\",\"day_length\":\"10:05:00.1530000\"},\"status\":\"OK\"}";
 
         [Test]
         public void ServiceSunsetCalculator_ImplementsISunsetCalculator()
         {
             //Arrange values to Initiate de TDD
             var srvSunsetCalculator = new ServiceSunsetCalculator();
-            Assert.IsInstanceOf<IServiceSunsetCalculator>(srvSunsetCalculator);
             
             //Actions and/or Methods
 
-
             //Assert
+            Assert.IsInstanceOf<IServiceSunsetCalculator>(srvSunsetCalculator);
+        }
 
+        [Test]
+        public void ParseJSonSunsetValu_OnGoodData_ReturnExpectedString()
+        {
+            //Arrange values  - Expected
+            string expected = "4:42:49 PM";
+            //Action
+            string result = ServiceSunsetCalculator.ParseSunset(goodData);
+            //Assert
+            Assert.AreEqual(expected,result);
         }
     }
 }
