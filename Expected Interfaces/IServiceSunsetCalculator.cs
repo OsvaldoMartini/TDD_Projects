38e3e3d564985f8aab65426c76de98f5ab6d1fcb
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-03-18 13:06:05 +0000

    TDD Red Cycle
    Defined two signatures in IServiceSunsetCalculator
    Not Implemented yet in ServiceSunsetCalculator class

--- Expected Interfaces/IServiceSunsetCalculator.cs
+++ Expected Interfaces/IServiceSunsetCalculator.cs
@@ -8,6 +8,8 @@ namespace Expected_Interfaces
 {
     public interface IServiceSunsetCalculator
     {
+        DateTime GetSunset(DateTime date);
+        DateTime GetSunrise(DateTime date);
 
     }
 }
