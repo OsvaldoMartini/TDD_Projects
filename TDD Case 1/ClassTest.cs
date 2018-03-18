8d47c2ff0fb77ebfcd700114119fea2ba21a546c
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-03-18 11:43:07 +0000

    NUnit and NUnit3TestAdapter
    Unistalled NuGet Older version (2.7)
    Installed NuGet 2.12 to allow install NUnit 3.10.1
    Installed NUnit 3.10.1
    Installed NUnit3TestAdapter (To Integrate in VS Test Explorer)

--- TDD Case 1/ClassTest.cs
+++ TDD Case 1/ClassTest.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace TDD_Case_1
 {
-    public class Class1
+    [TestFixture]
+    public class ReadeJsonFromWeb
     {
+
+        [Test]
+        public void SampleTest()
+        {
+
+        }
     }
 }
