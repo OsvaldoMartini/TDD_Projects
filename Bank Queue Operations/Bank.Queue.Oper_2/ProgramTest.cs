4690232aea1917eb971b888e394e55bdda815bd8
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-03-26 14:01:09 +0100

    Add Bank Queue Operations

---
+++ Bank Queue Operations/Bank.Queue.Oper_2/ProgramTest.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueExample1
+{
+    class ProgramTest
+    {
+        static void Main(string[] args)
+        {
+            QueueDemo ThomastonBankandTrust;
+            ThomastonBankandTrust = new QueueDemo();
+            ThomastonBankandTrust.QueueCustomers();
+            // pause
+            Console.Read();
+        }
+    }
+}
