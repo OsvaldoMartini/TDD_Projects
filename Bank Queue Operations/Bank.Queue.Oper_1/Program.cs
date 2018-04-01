effd348d2c2ff2c23c6a475e506b1fe420a0bc22
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-04-01 10:30:09 +0100

    Alter TellerLine for localBankQueue

--- Bank Queue Operations/Bank.Queue.Oper_1/Program.cs
+++ Bank Queue Operations/Bank.Queue.Oper_1/Program.cs
@@ -1,8 +1,9 @@
 using System;
+using Bank.Queue.Oper_1.Concrete;
 
 namespace Bank.Queue.Oper_1
 {
-    class ProgramTest
+    class Program
     {
         static void Main(string[] args)
         {
@@ -11,36 +12,36 @@ namespace Bank.Queue.Oper_1
             // create a customer
             BankCustomer JP = new BankCustomer("J P Morgan", Bank.BankingActivity.deposit.ToString(), 335445, 30000);
             // add the customer to the TellerLine
-            ThomastonBankandTrust.TellerLine.Enqueue(JP);
+            ThomastonBankandTrust.localBankQueue.Enqueue(JP);
             BankCustomer Butch = new BankCustomer("Butch Cassidy", Bank.BankingActivity.transferFunds.ToString(), 555445, 3500);
             BankCustomer Sundance = new BankCustomer("Sundance Kid", Bank.BankingActivity.withdrawl.ToString(), 555444, 3500);
             BankCustomer John = new BankCustomer("John Dillinger", Bank.BankingActivity.withdrawl.ToString(), 12345, 2000);
-            ThomastonBankandTrust.TellerLine.Enqueue(Sundance);
-            ThomastonBankandTrust.TellerLine.Enqueue(Butch);
-            ThomastonBankandTrust.TellerLine.Enqueue(John);
+            ThomastonBankandTrust.localBankQueue.Enqueue(Sundance);
+            ThomastonBankandTrust.localBankQueue.Enqueue(Butch);
+            ThomastonBankandTrust.localBankQueue.Enqueue(John);
 
-            Console.WriteLine("Peek: " + ThomastonBankandTrust.TellerLine.Peek());
+            Console.WriteLine("Peek: " + ThomastonBankandTrust.localBankQueue.Peek());
 
             // Pause
             Console.ReadLine();
-            ThomastonBankandTrust.QueuePeek(ThomastonBankandTrust.TellerLine);
+            ThomastonBankandTrust.QueuePeek(ThomastonBankandTrust.localBankQueue);
             // View the queue using a copy 
-            ThomastonBankandTrust.QueueContentsCopy(ThomastonBankandTrust.TellerLine);
+            ThomastonBankandTrust.QueueContentsCopy(ThomastonBankandTrust.localBankQueue);
             // view the queue through enumeration 
-            ThomastonBankandTrust.QueueContentsEnum(ThomastonBankandTrust.TellerLine);
+            ThomastonBankandTrust.QueueContentsEnum(ThomastonBankandTrust.localBankQueue);
 
             // Pause
             Console.ReadLine();
             // verify that the origional queue is not modified.
-            Console.WriteLine("Count of items in queue after copy & enum :" + ThomastonBankandTrust.TellerLine.Count.ToString());
+            Console.WriteLine("Count of items in queue after copy & enum :" + ThomastonBankandTrust.localBankQueue.Count.ToString());
             do
             {
                 BankCustomer nextInLine = new BankCustomer();
-                nextInLine = (BankCustomer)ThomastonBankandTrust.TellerLine.Dequeue();
+                nextInLine = (BankCustomer)ThomastonBankandTrust.localBankQueue.Dequeue();
                 ThomastonBankandTrust.ProcessCustomerRequest(nextInLine);
                 // Pause
                 Console.ReadLine();
-            } while (ThomastonBankandTrust.TellerLine.Count != 0);
+            } while (ThomastonBankandTrust.localBankQueue.Count != 0);
 
             // Pause
             Console.ReadLine();
